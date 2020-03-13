46b40a6495bfdfb16d4e87d2fe26239710ab7e0c
Aaron Beasley <aaroncb@aaroncb.com>
2020-03-13 10:29:16 -0500

    ADD MOTD Route, and Base Server.js File
    - Added initial MOTD Route, and base files required for full server functionality

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const projectsRouter = require("./projects/projectsRouter");
+const actionsRouter = require("./actions/actionsRouter");
+
+const server = express();
+
+//custom middleware
+
+function logger(req, _res, next) {
+  console.log(
+    `[${new Date().toISOString()}]: ${req.method} to ${req.url}`
+  );
+
+  next();
+}
+
+server.use(express.json());
+server.use(logger);
+// server.use("/api/projects", projectsRouter);
+// server.use("/api/actions", actionsRouter);
+
+module.exports = server;
